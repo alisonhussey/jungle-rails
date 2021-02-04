7c6b78bc7b7e577e9be1073b936af67f83659739
alisonhussey <alisonhussey@gmail.com>
2021-02-04 02:28:20 +0000

    added display counts of products and product categories to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class Admin::DashboardController < ApplicationController
 
-  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV[["HTTP_BASIC_PASSWORD"]
-  
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
+
   def show
+    @products_count = Product.count
+    @product_categories_count = Product.distinct.count(:category_id)
   end
 end
