792eaa8eed15f3848c873bf18ca12986e9e34483
alisonhussey <alisonhussey@gmail.com>
2021-02-09 21:33:53 +0000

    added rspec/capybara/poltergeist testing for add-to-cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  
+  #SETUP
+  before :each do
+    @category = Category.create! name: "Apparel"
+
+    10.times do |n|
+    @category.products.create(
+      name: Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+    end
+  end
+
+  scenario "they see all products" do
+    #ACT
+    visit root_path
+    expect(page).to have_content('My Cart (0)')
+    save_screenshot "test4.png"
+
+    first(:button, "Add").click
+
+    #DEBUG
+    save_screenshot "test5.png"
+    puts page.html
+
+    #VERIFY
+    expect(page).to have_content ("My Cart (1)") 
+
+  end
+end
