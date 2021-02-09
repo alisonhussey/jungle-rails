ebe870fe7d400e7d2527d99938cf102f07010906
alisonhussey <alisonhussey@gmail.com>
2021-02-09 01:29:47 +0000

    added minimum password length to user model, added all validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,6 +32,20 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
 
+    it "should have a unique email" do
+      @user1 = User.new(first_name: "Alison", last_name: "Hussey", email: "alisonhussey@gmail.com", password: "password", password_confirmation: "password")
+      @user1.save!
+      @user2 = User.new(first_name: "Alison", last_name: "Hussey", email: "ALISONHUSSEY@gmail.com", password: "password", password_confirmation: "password")
+      expect(@user2).to_not be_valid
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end
+    
+    it "should have a password that's at least 8 chars long" do
+      @user = User.new(first_name: "Alison", last_name: "Hussey", email: "alisonhussey@gmail.com", password: "pass", password_confirmation: "pass")
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+
 
   end
 end
