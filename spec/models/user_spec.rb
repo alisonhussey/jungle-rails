a8c3e6ad5625707afce8c593954e9357d7c5a1bb
alisonhussey <alisonhussey@gmail.com>
2021-02-09 01:02:52 +0000

    added tests for all fields in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,23 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
 
-    
+    it "should not save a user without a first name" do
+      @user = User.new(first_name: nil, last_name: "Hussey", email: "alisonhussey@gmail.com", password: "password", password_confirmation: "password")
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+  
+    it "should not save a user without a last name" do
+      @user = User.new(first_name: "Alison", last_name: nil, email: "alisonhussey@gmail.com", password: "password", password_confirmation: "password")
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
+
+    it "should not save a user without an email" do
+      @user = User.new(first_name: "Alison", last_name: "Hussey", email: nil, password: "password", password_confirmation: "password")
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
 
 
   end
